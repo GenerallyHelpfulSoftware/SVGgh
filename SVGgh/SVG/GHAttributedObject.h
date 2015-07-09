604fd6c876f1135f30c9aae31ad8a9c0f171d9e5
Glenn Howes <grhowes@mac.com>
2015-07-09 09:03:15 -0400

    Relying on NS_ASSUME_NONNULL_BEGIN

--- SVGgh/SVG/GHAttributedObject.h
+++ SVGgh/SVG/GHAttributedObject.h
@@ -39,7 +39,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface GHAttributedObject : NSObject
 @property (strong, nonatomic, readonly) NSDictionary*  	attributes;
 
--(nonnull instancetype) initWithDictionary:(NSDictionary*)theAttributes;
+-(instancetype) initWithDictionary:(NSDictionary*)theAttributes;
 -(instancetype) initWithAttributes:(NSDictionary*)theAttributes;
 
 
