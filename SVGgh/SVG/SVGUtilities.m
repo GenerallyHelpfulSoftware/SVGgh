e1e01cd3b8bae5a3052df4ba6e815304aac050e6
Glenn Howes <grhowes@mac.com>
2016-04-23 04:17:30 -0400

    stray ' ' in a color definition

--- SVGgh/SVG/SVGUtilities.m
+++ SVGgh/SVG/SVGUtilities.m
@@ -1128,7 +1128,7 @@ NSDictionary<NSString*, NSString*>* WebNameMapping()
           @"greenyellow":@"#adff2f",
           @"honeydew":@"#f0fff0",
           @"hotpink":@"#ff69b4",
-          @"indianred ":@"#cd5c5c",
+          @"indianred":@"#cd5c5c",
           @"indigo ":@"#4b0082",
           @"ivory":@"#fffff0",
           @"khaki":@"#f0e68c",
